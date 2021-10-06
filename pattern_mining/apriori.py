8a1f0d846f50fd8b80b2ba9fc08d52d499bc9aa4
Nate August <n.baker.august@gmail.com>
2021-10-06 13:35:51 -0400

    fix handling tuples or strings

--- pattern_mining/apriori.py
+++ pattern_mining/apriori.py
@@ -2,7 +2,9 @@ from typing import List, Union
 from math import floor
 
 
-def find_frequent(candidates: List[tuple], transactions, min_sup: int) -> dict:
+def find_frequent(
+    candidates: List[tuple], transactions, min_sup: int, prune_infrequent: bool = True
+) -> dict:
     """
     Counts transactions that contain all items in candidate for
     every candidate.
@@ -16,19 +18,27 @@ def find_frequent(candidates: List[tuple], transactions, min_sup: int) -> dict:
     Returns a tuple of (dict of {frequent tuples: counts}, frequent_transactions,)
     """
     counts = {}
-    frequent_transaction_idx = set()
-    for i, t in enumerate(transactions):
+    if prune_infrequent:
+        frequent_transactions = []
+
+    for t in transactions:
+        added = False
         for candidate in candidates:
             if all([c in t for c in candidate]):
                 counts[candidate] = counts.get(candidate, 0) + 1
-                frequent_transaction_idx.add(i)
+                if prune_infrequent and not added:
+                    frequent_transactions.append(t)
+                    added = True
     # Filter to only frequent
     output = {}
     for k, v in counts.items():
         if v >= min_sup:
             output[k] = v
+    if prune_infrequent:
+        return output, list(frequent_transactions)
 
-    return output, [transactions[i] for i in frequent_transaction_idx]
+    else:
+        return output, transactions
 
 
 def has_infrequent_subset(candidate: tuple, L_minus: List[tuple]):
@@ -51,8 +61,8 @@ def find_candidates(k: int, L_minus: dict) -> List[str]:
     candidates = []
     for i1 in L_minus.keys():
         for i2 in L_minus.keys():
-            if i1[: k - 1] == i2[: k - 1] and (i1[-1] > i2[-1]):
-                c = (i2[-1],) + i1
+            if i1[:-1] == i2[:-1] and (i1[-1] < i2[-1]):
+                c = i1 + (i2[-1],)
                 # Prune those with infrequent subset
                 if not has_infrequent_subset(c, L_minus.keys()):
                     candidates.append(c)
@@ -60,7 +70,9 @@ def find_candidates(k: int, L_minus: dict) -> List[str]:
 
 
 def apriori(
-    transactions: List[Union[List[str], tuple]], min_sup: Union[int, float]
+    transactions: List[Union[List[str], tuple]],
+    min_sup: Union[int, float],
+    prune_infrequent: bool = True,
 ) -> dict:
     """
     Apriori algorithm for mining frequent patterns as first proposed
@@ -76,25 +88,33 @@ def apriori(
         min_sup = floor(min_sup * len(transactions))
 
     L: List[str] = []
-
-    # Sort all transaction items
     transactions = [tuple(sorted(t)) for t in transactions]
 
     # Get unique items
     itemset = set()
     [itemset.update(set(t)) for t in transactions]
-    items = list(tuple(i) for i in itemset)  # convert back to tuples
-
-    L0, transactions = find_frequent(items, transactions, min_sup)
+    items = []
+    for i in itemset:
+        if isinstance(i, tuple):
+            items.append(i)
+        else:
+            items.append((i,))
+    L0, transactions = find_frequent(
+        items, transactions, min_sup, prune_infrequent=prune_infrequent
+    )
     L.append(L0)
-    for k in range(1, max([len(t) for t in transactions])):
+    max_tx_length = max([len(t) for t in transactions])
+    k = 1
+    while len(transactions) > 0 and k <= (max_tx_length + 1):
         candidates = find_candidates(k, L[k - 1])
-        l, transactions = find_frequent(candidates, transactions, min_sup)
+        l, transactions = find_frequent(
+            candidates, transactions, min_sup, prune_infrequent=prune_infrequent
+        )
         L.append(l)
+        k += 1
 
     output = {}
     for l in L:
         for k, v in l.items():
             output[k] = v
-
     return output
