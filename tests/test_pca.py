37f0331904f8cf0a632e2a4c6b2355e0b95d4395
Nate August <n.baker.august@gmail.com>
2021-11-09 15:12:49 -0500

    repo organization

--- tests/test_pca.py
+++ tests/test_pca.py
@@ -18,7 +18,7 @@ def test_PCA() -> None:
     pca.fit(data)
 
     new_X = pca.transform(2)
-    _, axarr = plt.subplots(1,2)
+    _, axarr = plt.subplots(1, 2)
     axarr[0].scatter(new_X[:, 0], new_X[:, 1], c=y)
     print(pca.singular_values)
     print(pca.components)
@@ -28,11 +28,10 @@ def test_PCA() -> None:
     print(skpca.singular_values_)
     print(skpca.components_)
     axarr[1].scatter(sk_new_X[:, 0], sk_new_X[:, 1], c=y)
-   
+
     plt.show()
     assert allclose(sk_new_X, new_X[:, :2])
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     test_PCA()
-
