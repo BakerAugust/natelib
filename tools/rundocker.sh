dd551f4e2b5b99fb3812ac7c96daef779d3becdf
Nate August <n.baker.august@gmail.com>
2021-12-02 15:23:18 -0500

    reorganize

--- tools/rundocker.sh
+++ tools/rundocker.sh
@@ -1,4 +1,4 @@
 cp ~/.pip/pip.conf .
 docker build -t myimage .
 rm pip.conf
-docker run -v ~/.aws:/root/.aws -v ~/repos/natelib/src/:/src -it myimage bash
+docker run -v ~/.aws:/root/.aws -v ~/repos/natelib/src/:/src -it myimage -p 8888:8888 bash
